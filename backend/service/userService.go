f9c8fbd126ee53d40cf143f8d102fb31bd37c797
Guilherme-Beckman <guilhermebeckman3@gmail.com>
2025-09-27 19:13:25 -0400

    feat: cria servico crud de usuarios

---
+++ backend/service/userService.go
@@ -0,0 +1,69 @@
+package service
+
+import (
+	"github.com/Guilherme-Beckman/eco-carona.git/model"
+	"gorm.io/gorm"
+)
+
+type UserService struct {
+	db *gorm.DB
+}
+
+func NewUserService(db *gorm.DB) *UserService {
+	return &UserService{
+		db: db,
+	}
+}
+
+// primeiro param é usado para usar o objeto dessa instancia
+// equivalente ao this
+// o segundo é o parametro que tem qeu ser passado
+// o terceiro sao os dois tipos de retorno
+func (s *UserService) FindById(id uint64) (model.User, error) {
+	// primeiro eu inicializo um ponteiro para um novo struct de user
+	// depois eu pesquiso no banco de dados ate achar o primeiro e passado
+	// o endereço da variavel como destino
+	user := new(model.User)
+	resp := s.db.First(user, id)
+	if resp.Error != nil {
+		return model.User{}, resp.Error
+	}
+
+	return *user, nil
+}
+
+func (s *UserService) SaveUser(user model.User) (model.User, error) {
+	result := s.db.Create(&user)
+	if result.Error != nil {
+		return model.User{}, result.Error
+	}
+	return user, nil
+}
+
+func (s *UserService) UpdateUser(user model.User) (model.User, error) {
+	exist, err := s.FindById(user.ID)
+	if err != nil {
+		return model.User{}, err
+	}
+
+	exist.Nome = user.Nome
+	exist.Turno = user.Turno
+	exist.Descricao = user.Descricao
+	exist.Idade = user.Idade
+
+	resp := s.db.Save(&exist)
+
+	if resp.Error != nil {
+		return model.User{}, resp.Error
+	}
+
+	return exist, nil
+}
+
+func (s *UserService) DeleteUser(id int64) error {
+	res := s.db.Delete(&model.User{}, id)
+	if res.Error != nil {
+		return res.Error
+	}
+	return nil
+}
